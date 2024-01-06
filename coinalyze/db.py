a408e599d730c9a8660ffe508029a0607d4d0a29
Avetius <avet.sargsyan@gmail.com>
2024-01-06 06:20:21 +0400

    dp.py tables are created on postgres db not on coinalyze

--- coinalyze/db.py
+++ coinalyze/db.py
@@ -1,5 +1,18 @@
+import os
 import psycopg2
 from psycopg2 import sql
+from dotenv import load_dotenv
+
+load_dotenv()
+host = os.getenv("POSTGRES_HOST")
+database_name = os.getenv("POSTGRES_DB")
+user = os.getenv("POSTGRES_USER")
+password = os.getenv("POSTGRES_PASSWORD")
+
+# print(f"host >>> {host}")
+# print(f"database_name >>> {database_name}")
+# print(f"user >>> {user}")
+# print(f"password >>> {password}")
 
 def create_timescaledb_database(database_name, user, password, host):
     # Connect to the default database (usually 'postgres')
@@ -10,6 +23,7 @@ def create_timescaledb_database(database_name, user, password, host):
         # Check if the database exists
         cursor.execute(sql.SQL("SELECT 1 FROM pg_database WHERE datname = %s"), (database_name,))
         if cursor.fetchone() is None:
+            print(f"No database {database_name} found")
             # Create a new database
             cursor.execute(sql.SQL("CREATE DATABASE {}").format(sql.Identifier(database_name)))
 
@@ -21,7 +35,111 @@ def create_timescaledb_database(database_name, user, password, host):
                 new_db_cursor.execute("CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;")
             new_db_conn.close()
 
+    with conn.cursor() as cursor:
+        # Create the exchange table if it doesn't exist
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS exchange (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR NOT NULL,
+                code VARCHAR NOT NULL
+            );
+        """)
+        # Convert the exchange table to a hypertable, if not already a hypertable
+        # cursor.execute("SELECT create_hypertable('exchange', 'code') ON CONFLICT DO NOTHING;")
+
+        # Create the future table if it doesn't exist
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS future (
+                id SERIAL PRIMARY KEY,
+                symbol VARCHAR NOT NULL,
+                exchange VARCHAR NOT NULL,
+                symbol_on_exchange VARCHAR NOT NULL,
+                base_asset VARCHAR NOT NULL,
+                quote_asset VARCHAR NOT NULL,
+                expire_at BIGINT,
+                has_buy_sell_data BOOLEAN NOT NULL,
+                is_perpetual BOOLEAN NOT NULL,
+                margined VARCHAR NOT NULL,
+                oi_lq_vol_denominated_in VARCHAR NOT NULL,
+                has_long_short_ratio_data BOOLEAN NOT NULL,
+                has_ohlcv_data BOOLEAN NOT NULL
+            );
+        """)
+
+        # Convert the future table to a hypertable, if not already a hypertable
+        # cursor.execute("SELECT create_hypertable('future', 'exchange') ON CONFLICT DO NOTHING;")
+
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS all_futures (
+                id SERIAL PRIMARY KEY,
+                symbol VARCHAR NOT NULL,
+                exchange VARCHAR NOT NULL,
+                symbol_on_exchange VARCHAR NOT NULL,
+                base_asset VARCHAR NOT NULL,
+                quote_asset VARCHAR NOT NULL,
+                expire_at BIGINT,
+                has_buy_sell_data BOOLEAN NOT NULL,
+                is_perpetual BOOLEAN NOT NULL,
+                margined VARCHAR NOT NULL,
+                oi_lq_vol_denominated_in VARCHAR NOT NULL,
+                has_long_short_ratio_data BOOLEAN NOT NULL,
+                has_ohlcv_data BOOLEAN NOT NULL
+            );
+        """)
+
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS gateio_future (
+                id SERIAL PRIMARY KEY,
+                symbol VARCHAR NOT NULL,
+                exchange_id INTEGER REFERENCES gateio_future(id)
+            );
+        """)
+
+        # Convert the future table to a hypertable, if not already a hypertable
+        # cursor.execute("SELECT create_hypertable('gateio_future', 'exchange_id')") #  ON CONFLICT DO NOTHING;
+
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS gateio_oi (
+                id SERIAL PRIMARY KEY,
+                symbol_id INTEGER REFERENCES gateio_future(id),
+                symbol VARCHAR NOT NULL,
+                value FLOAT NOT NULL,
+                update BIGINT NOT NULL
+            );
+        """)
+
+        # Convert the future table to a hypertable, if not already a hypertable
+        cursor.execute("SELECT create_hypertable('gateio_oi', 'update')")
+
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS gateio_price (
+                id SERIAL PRIMARY KEY,
+                symbol VARCHAR NOT NULL,
+                value FLOAT NOT NULL,
+                update BIGINT NOT NULL
+            );
+        """)
+
+        # Convert the future table to a hypertable, if not already a hypertable
+        cursor.execute("SELECT create_hypertable('gateio_price', 'update')")
+
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS gateio_delta (
+                id SERIAL PRIMARY KEY,
+                symbol_id INTEGER REFERENCES gateio_future(id),
+                symbol VARCHAR NOT NULL,
+                value FLOAT NOT NULL,
+                update BIGINT NOT NULL
+            );
+        """)
+
+        # Convert the future table to a hypertable, if not already a hypertable
+        cursor.execute("SELECT create_hypertable('gateio_delta', 'update')")
+
     conn.close()
 
-# Example usage
-create_timescaledb_database('my_new_timescale_db', 'my_user', 'my_password', 'localhost')
+
+
+if __name__ == '__main__':
+    # Example usage
+    create_timescaledb_database(database_name, user, password, host)
--- coinalyze/models.py
+++ coinalyze/models.py
@@ -1,5 +1,14 @@
-from sqlalchemy import create_engine, Column, Integer, Float, String, DateTime, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, Float, BigInteger, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+host = os.getenv("POSTGRES_HOST")
+database_name = os.getenv("POSTGRES_DB")
+user = os.getenv("POSTGRES_USER")
+password = os.getenv("POSTGRES_PASSWORD")
 
 Base = declarative_base()
 
@@ -20,18 +29,36 @@ class OI(Base):
     id = Column(Integer, primary_key=True)
     symbol_id = Column(Integer, ForeignKey('symbol.id'))
     value = Column(Float)
-    update = Column(DateTime)
-    
+    update = Column(BigInteger)
+
 class Price(Base):
     __tablename__ = 'price'
     id = Column(Integer, primary_key=True)
     symbol_id = Column(Integer, ForeignKey('symbol.id'))
     value = Column(Float)
-    update = Column(DateTime)
+    update = Column(BigInteger)
 
 class Delta(Base):
     __tablename__ = 'delta'
     id = Column(Integer, primary_key=True)
     symbol_id = Column(Integer, ForeignKey('symbol.id'))
     value = Column(Float)
-    update = Column(DateTime)
+    update = Column(BigInteger)
+
+
+if __name__ == '__main__':
+    # Example usage
+    engine = create_engine(f"postgres://{user}:{password}@{host}:5432/coinalyze")
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Creating and adding a new record
+    record = MyModel(large_number=12345678901234567890)
+    session.add(record)
+    session.commit()
+
+    # Querying the record
+    queried_record = session.query(MyModel).first()
+    print(queried_record.large_number)
